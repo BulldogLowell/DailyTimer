4dea4aadfdf5f13b9fee32a626bd99dcc75b8d47
Jim Brower <BulldogLowell@gmail.com>
2016-05-14 10:36:59 -0400

    Update example.ino
    Added a timer state indicator to Timer1

--- example.ino
+++ example.ino
@@ -2,9 +2,7 @@
 DailyTimer.h Library
 BulldogLowell@gmail.com
 April, 2016
-
 ** Library provides tools to set daily timers for control of devices such as lamps, appliances, etc. Developed primarilary for houshold presence simulation.
-
 ** Allows for setting ON and OFF times, days of the week (i.e. weekends, weekdays, Fridays) and the use of random on/off times using this Constructor:
    DailyTimer myTimer( START_HOUR, START_MINUTE, END_HOUR, END_MINUTE, DAYS_OF_WEEK, RANDOM or FIXED)
    
@@ -12,7 +10,6 @@ April, 2016
    DailyTimer myTimer( 18, 30,  1, 45, WEEKENDS, FIXED);  // starts at 6:30pm Saturdays and Sundays and ends at 1:45am the next day.
    
 ** Automatically sets correct timer state on powerup, using isActive() in loop(), see example below.
-
 ** Random start and/or end times using this member function:
    myTimer.setRandomOffset(30, RANDOM_START);  //  Can be RANDOM_START, RANDOM_END, or both (RANDOM)  default random offfset is 15min
    
@@ -40,20 +37,20 @@ April, 2016
 #include "DailyTimer.h"
 #include <Time.h>
 
-DailyTimer timer1( 14, 37, 14, 39, EVERY_DAY, FIXED, customSeedGenerator);   // optional callback function for random number generation, see below example
-DailyTimer timer2( 14, 38, 20, 30, SATURDAY, RANDOM_START);                   // default is FIXED, this will randomize the start time only
-DailyTimer timer3( 14, 45,  1, 30, WEEKENDS);                                 // creates with a fixed start time and end time
+DailyTimer timer1( 14, 43, 14, 55, EVERY_DAY, RANDOM, customSeedGenerator);   // optional callback function for random number generation, see below example
+DailyTimer timer2( 14, 38, 23, 59, SATURDAY, FIXED);                          // default is FIXED, this will randomize the start time only
+DailyTimer timer3( 14, 39,  0,  1, WEEKENDS);                                 // creates with a fixed start time and end time
 bool timer1_LastState = false;
 
 void setup() 
 {
-  setTime(1461422271);                        // set your sync provider!!
+  setTime(1461422261);                        // set your sync provider!!
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  timer1.setDaysActive(WEEKENDS);             // Set the timer to be active on weekends
+  //timer1.setDaysActive(WEEKENDS);           // Set the timer to be active on weekends
   //timer1.setDaysActive(B10101010);          // or define a custom day mask... SMTWTFS0
   //timer1.setRandomDays(4);                  // or four random days per week
-  //timer2.setRandomOffset(30, RANDOM_START); // Change to random start time, +/- 30 minutes... max 59 mins
+  timer2.setRandomOffset(5, RANDOM);          // Change to random start time, +/- 5 minutes... max 59 mins
   Serial.println(timer1.getDays(), BIN);      // getDays() returns active days as a byte in the format above
   Serial.print(F("Today is "));
   Serial.println(dayStr(weekday()));
@@ -63,6 +60,7 @@ uint32_t lastUpdateTime = 0;
 
 void loop() 
 {
+  
   bool timerState = timer1.isActive();
   if(timerState != timer1_LastState)
   {
@@ -82,6 +80,11 @@ void loop()
   {
     Serial.println(F("Timer 2 FIRED!"));
   }
+  
+  if(timer3.startTrigger())
+  {
+    Serial.println(F("Timer 3 FIRED!"));
+  }
   if(millis() - lastUpdateTime > 1000UL)
   {
     char timeBuffer[32] = "";
@@ -89,14 +92,14 @@ void loop()
     Serial.println(timeBuffer);
     lastUpdateTime += 1000UL;
   }
-  if(timer3.startTrigger())
-  {
-    Serial.println(F("Timer 3 FIRED!"));
-  }
+  Serial.print(F("Timer1 is: "));
+  Serial.println(timerState? "Active" : "Inactive");
+  delay(1000);
 }
 
 uint32_t customSeedGenerator()
 {
+  Serial.println(F("New Seed Created"));
   uint32_t seed = analogRead(A5);
   delay(100);
   seed << 8;
