b010fe76b5c044149f626bd41babf8d83ca34b65
Jim Brower <BulldogLowell@gmail.com>
2017-05-28 12:11:42 -0400

    Modified Constructors to add one-edge Timer

--- DailyTimer.cpp
+++ DailyTimer.cpp
@@ -5,9 +5,7 @@
    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
    to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
    and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
@@ -30,48 +28,44 @@
 
 #include "DailyTimer.h"
 
-int DailyTimer::instanceCount = 0;
+uint8_t DailyTimer::instanceCount = 0;
 DailyTimer* instances[MAX_TIMER_INSTANCES] = {nullptr};
 
-//DailyTimer::DailyTimer(bool syncOnPowerup, byte StartHour, byte StartMinute, EventDays DaysOfTheWeek, void(*StartTimeCallback)())
-//{
-//  DailyTimer(syncOnPowerup, StartHour, StartMinute, 0, 0, DaysOfTheWeek, FIXED, StartTimeCallback, NULL);
-//}
 
-//DailyTimer::DailyTimer(bool syncOnPowerup, byte StartHour, byte StartMinute, EventDays DaysOfTheWeek, RandomTime randomTime, void(*StartTimeCallback)())
-//{
-//  DailyTimer(syncOnPowerup, StartHour, StartMinute, 0, 0, DaysOfTheWeek, randomTime, StartTimeCallback, NULL);
-//}
-
-DailyTimer::DailyTimer(bool syncOnPowerup, uint8_t StartHour, uint8_t StartMinute, uint8_t EndHour, uint8_t EndMinute, EventDays DaysOfTheWeek, void(*StartTimeCallback)(), void(*EndTimeCallback)())
+DailyTimer::DailyTimer(byte StartHour, byte StartMinute, EventDays DaysOfTheWeek, RandomType type, void(*StartTimeCallback)())
 {
-  autoSync = syncOnPowerup;
-  startTime.hour = StartHour % 24;
-  startTime.minute = StartMinute % 60;
-  endTime.hour = EndHour % 24;
-  endTime.minute = EndMinute % 60;
-  setDaysActive(dayTemplate[DaysOfTheWeek]);
-  randomType = FIXED;
-  offset = 0;
+  autoSync = false;
+  startTime.hour = StartHour > 23 ? 23 : StartHour; 
+  startTime.minute = StartMinute > 59 ? 59 : StartMinute; 
+  endTime.hour = startTime.hour;
+  endTime.minute = startTime.minute;
+  setDaysActive(dayTemplate[static_cast<int>(DaysOfTheWeek)]);
+  randomType = type;
+  offset = 15;
   startTimeCallback = StartTimeCallback;
-  endTimeCallback = EndTimeCallback;
-  instances[instanceCount++] = this;
-  //DailyTimer(syncOnPowerup, StartHour, StartMinute, EndHour, EndMinute, DaysOfTheWeek, FIXED, StartTimeCallback, EndTimeCallback);
+  endTimeCallback = NULL;
+  instances[instanceCount] = this;
+  instanceCount++;
 }
 
-DailyTimer::DailyTimer(bool syncOnPowerup, uint8_t StartHour, uint8_t StartMinute, uint8_t EndHour, uint8_t EndMinute, EventDays DaysOfTheWeek, RandomTime randomTime, void(*StartTimeCallback)(), void(*EndTimeCallback)())
+DailyTimer::DailyTimer(bool syncOnPowerup, byte StartHour, byte StartMinute, byte EndHour, byte EndMinute, EventDays DaysOfTheWeek, RandomType type, void(*StartTimeCallback)(), void(*EndTimeCallback)())
 {
   autoSync = syncOnPowerup;
-  startTime.hour = StartHour % 24;
-  startTime.minute = StartMinute % 60;
-  endTime.hour = EndHour % 24;
-  endTime.minute = EndMinute % 60;
-  setDaysActive(dayTemplate[DaysOfTheWeek]);
-  randomType = randomTime;
+  startTime.hour = StartHour > 23 ? 23 : StartHour; 
+  startTime.minute = StartMinute > 59 ? 59 : StartMinute; 
+  endTime.hour = EndHour > 23 ? 23 : EndHour;
+  endTime.minute = EndMinute > 59 ? 59 : EndMinute;
+  setDaysActive(dayTemplate[static_cast<int>(DaysOfTheWeek)]);
+  randomType = type;
   offset = 15;
   startTimeCallback = StartTimeCallback;
   endTimeCallback = EndTimeCallback;
   instances[instanceCount++] = this;
+}
+
+int DailyTimer::getInstanceCount(void) const
+{
+  return instanceCount;
 }
 
 bool DailyTimer::begin()
@@ -81,7 +75,7 @@ bool DailyTimer::begin()
 
 void DailyTimer::setDaysActive(EventDays days)
 {
-  onMask = dayTemplate[days];
+  onMask = dayTemplate[static_cast<int>(days)];
   time_t now_time = now();
   if(tmConvert_t(year(now_time), month(now_time), day(now_time), startTime.hour, startTime.minute, 0) > tmConvert_t(year(now_time), month(now_time), day(now_time), endTime.hour, endTime.minute, 0))
   {
@@ -109,49 +103,49 @@ void DailyTimer::setDaysActive(byte activeDays)
   (void)sync();
 }
 
-void DailyTimer::setRandomOffset(int random_minutes, RandomTime randomSetting)
+void DailyTimer::setRandomOffset(uint8_t random_minutes, RandomType type)
 {
-  offset = constrain(random_minutes, 0, 59);
+  offset = random_minutes > 59 ? 59 : random_minutes;
   if (offset == 0)
   {
     randomType = FIXED;
   }
   else
   {
-    randomType = randomSetting;
+    randomType = type;
   }
 }
 
-void DailyTimer::setStartTime(byte hour, byte minute)
+void DailyTimer::setStartTime(uint8_t hour, uint8_t minute)
 {
-  startTime.hour = hour % 24;
-  startTime.minute = minute % 60;
+  startTime.hour = hour > 23 ? 23 : hour; 
+  startTime.minute = minute > 59 ? 59 : minute; 
   setDaysActive(onMask);
   (void)sync();
 }
 
-void DailyTimer::setEndTime(byte hour, byte minute)
+void DailyTimer::setEndTime(uint8_t hour, uint8_t minute)
 {
-  endTime.hour = hour % 24;
-  endTime.minute = minute % 60;
+  endTime.hour = hour > 23 ? 23 : hour;
+  endTime.minute = minute > 59 ? 59 : minute;
   setDaysActive(onMask);
   (void)sync();
 }
 
 
-byte DailyTimer::setRandomDays(byte number_Days)
+uint8_t DailyTimer::setRandomDays(uint8_t number_Days)
 {
   randomSeed(now() + micros());
-  byte mask = 0;
-  byte array[8] = {0};
+  uint8_t mask = 0;
+  uint8_t array[8] = {0};
   for (int i = 0; i < number_Days; i++)
   {
     array[i] = 1;
   }
   for(int i = 0; i < 7; i++)
   {
-    byte index = random(i, 7);
-    byte temp = array[i];
+    uint8_t index = random(i, 7);
+    uint8_t temp = array[i];
     array[i] = array[index];
     array[index] = temp;
   }
@@ -164,7 +158,7 @@ byte DailyTimer::setRandomDays(byte number_Days)
   return onMask;
 }
 
-byte DailyTimer::getDays()
+byte DailyTimer::getDays() const
 {
   return onMask;
 }
@@ -189,11 +183,11 @@ void DailyTimer::update()
     {
       if(instances[i]->state == true)
       {
-        instances[i]->startTimeCallback();
+        if(instances[i]->startTimeCallback) instances[i]->startTimeCallback();
       }
       else
       {
-        instances[i]->endTimeCallback();
+        if(instances[i]->endTimeCallback) instances[i]->endTimeCallback();
       }
     }
   }
@@ -239,10 +233,18 @@ bool DailyTimer::isActive(DailyTimer* instance)
     {
       return (now_time > on_time || now_time < off_time);
     }
-    else // if both on and off are set to the same time, I'm confused... so let's do nothing
+//    else 
+//    {
+//      return false;
+//    }
+    else if(on_time == off_time) // single edge event
     {
-      return false;
+      if( now_time == on_time)
+      {
+        return true;
+      }
     }
+    return false;
   }
   else if (today & instance->onMask) // if it is supposed to turn only on today
   {
